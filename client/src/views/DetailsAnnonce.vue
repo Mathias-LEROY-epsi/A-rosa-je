623e7a1fbea4b2b65fd4f5eebb85649b187925ce
Mathias LEROY <mathias.leroy@epsi.fr>
2023-07-11 15:57:44 +0200

    correctifs sur les requetes

--- client/src/views/DetailsAnnonce.vue
+++ client/src/views/DetailsAnnonce.vue
@@ -201,6 +201,7 @@ export default {
               })
               .then(swal("Bien joué!", "message envoyé!", "success"))
               .then(() => {
+                this.commentaires = [];
                 window.location.reload();
               })
               .catch((err) => {
